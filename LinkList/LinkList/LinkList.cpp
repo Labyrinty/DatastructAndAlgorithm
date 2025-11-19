6942c033a5caa6cb0f3efd81e7a38924aaff7439
Labyrinty <2144973442@qq.com>
2025-11-19 22:13:15 +0800

    完善了一些功能，略微提升了可读性

--- LinkList/LinkList/LinkList.cpp
+++ LinkList/LinkList/LinkList.cpp
@@ -16,117 +16,154 @@ public:
 template <class T>
 class SingleLinkList {
 public:
-	SingleLinkList() : head(nullptr) {};
+	void InsertToHead(T val);
+	void InsertToTail(T val);
+	void Insert(T val, int index);
+	void traverse();
+	void DeleteNode(int index);
+	void ClearList();
+	void Reverse();
+	int GetLength();
+	T GetNodeValue(int index);
+private:
+	SingleLinkListNode<T>* head = nullptr;
+};
 
-	void InsertToHead(T val) {
-		SingleLinkListNode<T>* temp = new SingleLinkListNode<T>(val);
-		temp->next = head;
-		head = temp;
+template<class T>
+void SingleLinkList<T>::InsertToHead(T val) {
+	SingleLinkListNode<T>* temp = new SingleLinkListNode<T>(val);
+	temp->next = head;
+	head = temp;
+}
+
+template<class T>
+void SingleLinkList<T>::InsertToTail(T val) {
+	SingleLinkListNode<T>* temp1 = new SingleLinkListNode<T>(val);
+	SingleLinkListNode<T>* temp2 = head;
+	if (head == nullptr) {
+		head = temp1;
+		return;
+	}
+	while (temp2->next) {
+		temp2 = temp2->next;
+	}
+	temp2->next = temp1;
+}
+
+template<class T>
+void SingleLinkList<T>::Insert(T val, int index) {
+	if (index == 1) {
+		InsertToHead(val);
+		return;
+	}
+	else if (index == 0) {
+		std::cout << "It'ld start for 1";
+		return;
 	}
 
-	void InsertToTail(T val) {
-		SingleLinkListNode<T>* temp1 = new SingleLinkListNode<T>(val);
-		SingleLinkListNode<T>* temp2 = head;
-		if (head == nullptr) {
-			head = temp1;
-			return;
-		}
-		while (temp2->next) {
-			temp2 = temp2->next;
-		}
-		temp2->next = temp1;
+	if (index > GetLength()) {
+		std::cout << "Index out of bounds\n";
+		return;
 	}
 
-	void Insert(T val, int index) {
-		if (index == 0) {
-			InsertToHead(val);
-			return;
-		}
-		SingleLinkListNode<T>* temp1 = new SingleLinkListNode<T>(val);
-		SingleLinkListNode<T>* temp2 = head;
-		for (int i = 0; i < index - 1; i++) {
-			if (temp2 == nullptr) {
-				std::cout << "Index out of bounds\n";
-				delete temp1;
-				return;
-			}
-			temp2 = temp2->next;
-		}
-		temp1->next = temp2->next;
-		temp2->next = temp1;
+	SingleLinkListNode<T>* temp1 = new SingleLinkListNode<T>(val);
+	SingleLinkListNode<T>* temp2 = head;
+	for (int i = 0; i < index - 1; i++) {
+		temp2 = temp2->next;
+	}
+	temp1->next = temp2->next;
+	temp2->next = temp1;
+}
+
+template<class T>
+void SingleLinkList<T>::traverse() {
+	SingleLinkListNode<T>* current = head;
+	std::cout << "List is:";
+	while (current) {
+		std::cout << ' ' << current->val;
+		current = current->next;
+	}
+	std::cout << '\n';
+}
+
+template<class T>
+int SingleLinkList<T>::GetLength() {
+	SingleLinkListNode<T>* current = head;
+	int length = 0;
+	while (current) {
+		length++;
+		current = current->next;
+	}
+	return length;
+}
+
+template<class T>
+void SingleLinkList<T>::DeleteNode(int index) {
+	if (head == nullptr) {
+		std::cout << "List is empty\n";
+		return;
+	}
+	if (index > GetLength()) {
+		std::cout << "Index out of bounds\n";
+		return;
+	}
+	else if (index == 0) {
+		std::cout << "Index starts from 1\n";
+		return;
 	}
 
-	void traverse(){
-		SingleLinkListNode<T>* current = head;
-		std::cout << "List is:";
-		while (current) {
-			std::cout << ' ' << current->val;
-			current = current->next;
-		}
-		std::cout << '\n';
+	if (index == 1) {
+		SingleLinkListNode<T>* temp = head;
+		head = head->next;
+		delete temp;
+		return;
 	}
-
-	int GetLength() {
-		SingleLinkListNode<T>* current = head;
-		int length = 0;
-		while (current) {
-			length++;
-			current = current->next;
-		}
-		return length;
+	SingleLinkListNode<T>* current = head;
+	for (int i = 0; i < index - 2; i++) {
+		current = current->next;
 	}
+	SingleLinkListNode<T>* temp = current->next;
+	current->next = temp->next;
+	delete temp;
+}
 
-	void DeleteNode(int index) {
-		if (head == nullptr) {
-			std::cout << "List is empty\n";
-			return;
-		}
-		if (index > GetLength() - 1) {
-			std::cout << "Index out of bounds\n";
-			return;
-		}else if (index == 0) {
-			std::cout << "Index starts from 1\n";
-			return;
-		}
+template<class T>
+void SingleLinkList<T>::Reverse() {
+	SingleLinkListNode<T> *current, *prev, *next;
+	current = head;
+	prev = nullptr;
+	while (current) {
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	head = prev;
+}
 
-		if (index == 1) {
-			SingleLinkListNode<T>* temp = head;
-			head = head->next;
-			delete temp;
-			return;
-		}
-		SingleLinkListNode<T>* current = head;
-		for (int i = 0; i < index - 2; i++) {
-			current = current->next;
-		}
-		SingleLinkListNode<T>* temp = current->next;
-		current->next = temp->next;
+template<class T>
+T SingleLinkList<T>::GetNodeValue(int index) {
+	if (index >= GetLength()) {
+		std::cout << "Index out of bounds\n";
+		return T();
+	}
+	SingleLinkListNode<T>* current = head;
+	for (int i = 0; i < index; i++) {
+		current = current->next;
+	}
+	return current->val;
+}
+
+template<class T>
+void SingleLinkList<T>::ClearList() {
+	SingleLinkListNode<T>* current = head;
+	while (current) {
+		SingleLinkListNode<T>* temp = current;
+		current = current->next;
 		delete temp;
 	}
-
-	T GetNodeValue(int index) {
-		if (index >= GetLength()) {
-			std::cout << "Index out of bounds\n";
-			return T();
-		}
-		SingleLinkListNode<T>* current = head;
-		for (int i = 0; i < index; i++) {
-			current = current->next;
-		}
-		return current->val;
-	}
-	void ClearList() {
-		SingleLinkListNode<T>* current = head;
-		while (current) {
-			SingleLinkListNode<T>* temp = current;
-			current = current->next;
-			delete temp;
-		}
-		head = nullptr;
-	}
-private:
-	SingleLinkListNode<T>* head;
-};
+	head = nullptr;
+}
 
 int main() {
 	SingleLinkList <const char*> List;
@@ -134,8 +171,16 @@ int main() {
 	List.InsertToHead("world");
 	List.InsertToHead("Hello");
 	List.InsertToTail("My name is Yangcy");
-	List.Insert("Woooh~", 0);
+	List.Insert("Wooh~", 4);
 	List.traverse();
 	List.ClearList();
 	List.traverse();
+	SingleLinkList<int> numList;
+	numList.InsertToHead(2);
+	numList.InsertToHead(1);
+	numList.InsertToHead(0);
+	numList.InsertToHead(-1);
+	numList.traverse();
+	numList.Reverse();
+	numList.traverse();
 }
